ce3a1af3bc616aae499430c0267a2e2665c6f2a4
beepobb <yeejiazhen23@gmail.com>
2023-11-24 13:12:10 +0800

    finished home.html

--- flask-server/app/route.py
+++ flask-server/app/route.py
@@ -3,20 +3,38 @@ from app import webApp
 
 # landing page
 @webApp.route('/')
-@webApp.route('/landing')
-def landing():
+@webApp.route('/home')
+def home():
     title = 'Welcome'
     url = url_for('static', filename="images/landing_bg.png")
-    return render_template("landing.html", title=title, url=url)
+    return render_template("home.html", title=title, url=url)
 
-@webApp.route('/welcome')
-def welcome():
-    title = "Prediction Model"
-    url = url_for('static', filename="images/old_landing_bg.png")
-    return render_template("welcome.html", title=title, url=url)
+@webApp.route('/about')
+def about():
+    title = "About"
+    url = url_for('static', filename="images/BlankBlue.png")
+    return render_template("about.html", title=title, url=url)
+
+@webApp.route('/ml-model')
+def ml_model():
+    title = "Machine Learning Model"
+    url = url_for('static', filename="images/BlankBlue.png")
+    return render_template("model.html", title=title, url=url)
+
+@webApp.route('/tool')
+def tool():
+    title = "Tool"
+    url = url_for('static', filename="images/BlankBlue.png")
+    return render_template("tool.html", title=title, url=url)
+
+@webApp.route('/math-analysis')
+def math_analysis():
+    title = "Math Analysis"
+    url = url_for('static', filename="images/BlankBlue.png")
+    return render_template("math-analysis.html", title=title, url=url)
 
 @webApp.route('/history')
 def history():
     title = "History"
-    url = url_for('static', filename="images/BlankBlue.png")
+    url = url_for('static', filename="images/old_landding_bg.png")
     return render_template("history.html", title=title, url=url)
---
+++ flask-server/app/static/my-js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    hljs.highlightAll();
+})
+
+// var i = 0;
+// var txt = document.getElementById("tool-txt");
+// var speed = 50;
+
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("demo").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
