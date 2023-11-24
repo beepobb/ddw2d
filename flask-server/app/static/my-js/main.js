06c7e63e2c090562f53809b1ec987f683014b846
beepobb <yeejiazhen23@gmail.com>
2023-11-24 15:36:41 +0800

    added js for tool.html

--- flask-server/app/static/my-js/main.js
+++ flask-server/app/static/my-js/main.js
@@ -1,15 +1,42 @@
-document.addEventListener('DOMContentLoaded', () => {
-    hljs.highlightAll();
-})
+document.addEventListener('DOMContentLoaded', function() {
+    if (window.location.pathname === '/home' || window.location.pathname === '/') {
+        var newYearElement = document.getElementById('new-year');
+        var betterWorldElement = document.getElementById('better-world');
+        
+        // Delay the fade-in for "Better World" by 2 seconds (2000 milliseconds)
+        setTimeout(function() {
+            betterWorldElement.style.animationDelay = '0s';
+            betterWorldElement.classList.add('fade-in-text');
+            betterWorldElement.style.opacity = 1;
+        }, 1000);
 
-// var i = 0;
-// var txt = document.getElementById("tool-txt");
-// var speed = 50;
+        document.querySelectorAll('.btn-box').forEach(function(btnboxElement) {
+            setTimeout(function() {
+                btnboxElement.style.animationDelay = '1s';
+                btnboxElement.classList.add('fade-in-up');
+            }, 950);
+        })
+        // Start the fade-in for "New Year"
+        newYearElement.classList.add('fade-in-text');
+    }
+        
 
-// function typeWriter() {
-//   if (i < txt.length) {
-//     document.getElementById("demo").innerHTML += txt.charAt(i);
-//     i++;
-//     setTimeout(typeWriter, speed);
-//   }
-// }
+    if (window.location.pathname === '/about' || window.location.pathname === '/') {
+        var aboutElement = document.getElementById('about');
+
+        setPaddingClass();
+
+        // Check the window size on page load
+        function setPaddingClass() {
+            if (window.innerWidth < 1000) {
+                // If the window size is less than 1000 pixels, add the class
+                aboutElement.classList.remove('py-md-5');
+                aboutElement.classList.add('p-md-5');
+            } else {
+                aboutElement.classList.remove('p-md-5');
+                aboutElement.classList.add('py-md-5');
+            }
+        }
+    }
+});
+
