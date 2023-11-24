cb55ed7bdccbeb901267d05792c33771c6e72746
beepobb <yeejiazhen23@gmail.com>
2023-11-24 08:09:56 +0800

    deleted bootstrap-css and bootstrap-js to resolve error

--- flask-server/app/__init__.py
+++ flask-server/app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap5
+import os
 
-webApp = Flask(__name__, static_folder='static')
+
+webApp = Flask(__name__, static_url_path='/flask-server/app/static')
 bootstrap = Bootstrap5(webApp)
+
+# error handling
+if os.environ.get('FLASK_ENV') == 'production':
+    print("Running in production environment.")
+else:
+    print(f"Running in {os.environ.get('FLASK_ENV', 'development')} environment.")
 
 from app import route
 
--- flask-server/app/route.py
+++ flask-server/app/route.py
@@ -1,13 +1,22 @@
-from flask import render_template
+from flask import render_template, url_for
 from app import webApp  
+
 # landing page
 @webApp.route('/')
+@webApp.route('/landing')
+def landing():
+    title = 'Welcome'
+    url = url_for('static', filename="images/landing_bg.png")
+    return render_template("landing.html", title=title, url=url)
+
 @webApp.route('/welcome')
 def welcome():
     title = "Prediction Model"
-    return render_template("welcome.html", title=title)
+    url = url_for('static', filename="images/old_landing_bg.png")
+    return render_template("welcome.html", title=title, url=url)
 
 @webApp.route('/history')
 def history():
     title = "History"
-    return render_template("history.html", title=title)
+    url = url_for('static', filename="images/BlankBlue.png")
+    return render_template("history.html", title=title, url=url)
